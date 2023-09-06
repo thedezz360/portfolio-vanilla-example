c38cfb5b12c6d3eb5200b2e0941821dd0416762f
Daniel <thedezz360@gmail.com>
2023-09-06 16:11:12 +0200

    finish

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function start(){
   document.getElementById('menu-curriculum').addEventListener('click',seleccionar)
   document.getElementById('menu-proyectos').addEventListener('click',seleccionar)
   document.getElementById('nav-responsive').addEventListener('click',mostrarOcultarMenu)
-
+  // cuando scrolling aplicamos el efecto
+  window.addEventListener('scroll', efectoHabilidades)
 }
 
 let menuVisible=false
@@ -26,6 +27,34 @@ function mostrarOcultarMenu() {
   }else{
     document.getElementById('nav').classList='responsive'
     menuVisible=true
+  }
+}
+
+// aplica las animaciones a las skills
+// detectamos scrolling para aplicar la animacion
+function efectoHabilidades(){
+  
+  const arrHabilidades = ['javascript', 'htmlcss', 'php', 'react', 'java', 'node', 
+                          'comunicacion', 'trabajoEquipo', 'creatividad', 'dedicacion', 'projectManagement' ]
+  const skills = document.getElementById('skills')
+  
+  // calculamos en donde se encuentra la seccion skills
+  const distanciaSkills = window.innerHeight - skills.getBoundingClientRect().top
+
+  const habilidades = document.getElementsByClassName('progreso')
+
+  if(distanciaSkills >= 300 && distanciaSkills <= 1400){
+
+    for (let i = 0; i < habilidades.length; i++) {
+      const habilidad = habilidades[i];
+      habilidad.classList.add(arrHabilidades[i])
+    }
+    
+  }else{
+    for (let i = 0; i < habilidades.length; i++) {
+      const habilidad = habilidades[i];
+      habilidad.classList.remove(arrHabilidades[i])
+    }
   }
 }
 
